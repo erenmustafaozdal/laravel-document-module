d42fc423df690d1fcafb777b2e517818d5307a01
erenMustafaOzdal <eren.060737@gmail.com>
2016-08-24 04:26:17 +0000

    file

--- src/Http/Requests/Document/StoreRequest.php
+++ src/Http/Requests/Document/StoreRequest.php
@@ -35,8 +35,8 @@ class StoreRequest extends Request
             ? "required|max:{$max}|mimes:{$mimes}"
             : "required";
         $photoValidation = $this->has('photo') && $this->file('photo')
-            ? "required|max:{$max_photo}|image|mimes:{$mimes_photo}"
-            : "required";
+            ? "max:{$max_photo}|image|mimes:{$mimes_photo}"
+            : "";
         return [
             'category_id'       => 'required|integer',
             'title'             => 'required|max:255',
